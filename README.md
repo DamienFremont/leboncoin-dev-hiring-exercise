3f73f50f85241099151ce1d635ff7819e9ec5be3
Damien Frémont <damien.fremont@gmail.com>
2022-10-18 22:46:55 +0200

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
+LE BON COIN - DEV HIRING EXERCISE
+=================================
+
+Base sur les specs [SPECS.md](SPECS.md)
+
+---
+
+## Requirements
+
+- Install Java 11
+- Configure JAVA_HOME
+
+---
+
+## Getting Started
+
+```bash
+.\mvnw clean package
+```
+
+## Usage
+
+```bash
+.\mvnw spring-boot:run 
+```
+
+---
+
+## Resources
+
+GitLab
+
 ### Java Spring template project
 
 This project is based on a GitLab [Project Template](https://docs.gitlab.com/ee/gitlab-basics/create-project.html).
--- src/test/java/com/example/demo/ExerciseApplicationTests.java
+++ src/test/java/com/example/demo/ExerciseApplicationTests.java
@@ -1,17 +1,15 @@
 package com.example.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class DemoApplicationTests {
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class ExerciseApplicationTests {
 
 	@Test
 	public void contextLoads() {
